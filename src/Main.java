27ccff57561fd8b62c1de31981a6c7f936c0b37a
Sajedeh Panahi <sajedehpanahi@gmail.com>
2016-05-31 16:57:42 +0430

    a

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import org.xml.sax.SAXException;
+
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.List;
+import java.math.BigDecimal;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SAXException {
         XMLFileParser readXMLFile = new XMLFileParser();
-        readXMLFile.read();
+        List<Deposit> depositResults = readXMLFile.read();
+        for (Deposit depositResult : depositResults) {
+            System.out.println("CustomerNumber: " + depositResult.getCustomerNumber() + "    PayedInterest: " + depositResult.calculatePayedInterest());
+        }
     }
 }
