93449b70f4ccc9ffdd70f3c50db5c115b1be4151
Sajedeh Panahi <sajedehpanahi@gmail.com>
2016-06-08 14:49:48 +0430

    a

--- src/XMLFileParser.java
+++ src/XMLFileParser.java
@@ -1,3 +1,4 @@
+import exception.NegativeBalanceException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,119 +10,72 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLFileParser {
 
-    public Document read() throws IOException, SAXException,
+    public List<Deposit> read() throws IOException, SAXException,
             ParserConfigurationException, ClassNotFoundException, IllegalAccessException, InstantiationException {
 
-        File xmlFile = new File("src/sampleXML.xml");
+        File xmlFile = new File("sampleXML.xml");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(xmlFile);
-
         doc.getDocumentElement().normalize();
-
-        Element root = doc.getDocumentElement();
-        System.out.println("root is:" + root.getNodeName());
 
         NodeList listOfDeposits = doc.getElementsByTagName("deposit");
 
-        for (int i = 0; i < 3; i++) {
+        List<Deposit> listForSavingDeposits = new ArrayList<Deposit>();
+
+        for (int i = 0; i < listOfDeposits.getLength(); i++) {
             Node nodeForEachDeposit = listOfDeposits.item(i);
             if (nodeForEachDeposit.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nodeForEachDeposit;
-//                System.out.println("customername : "    + eElement.getAttribute("customerNumber"));
-//                System.out.println("depositType : "  + eElement.getElementsByTagName("depositType").item(0).getTextContent());
-//                System.out.println("depositbalance : "   + eElement.getElementsByTagName("depositBalance").item(0).getTextContent());
-//                System.out.println("durationindays : "    + eElement.getElementsByTagName("durationInDays").item(0).getTextContent());
+                String depositType = String.valueOf(eElement.getElementsByTagName("depositType").item(0).getTextContent());
+                String customerNumber = String.valueOf(eElement.getElementsByTagName("customerNumber").item(0).getTextContent());
+                String depositBalance = String.valueOf(eElement.getElementsByTagName("depositBalance").item(0).getTextContent());
+                String durationInDays = String.valueOf(eElement.getElementsByTagName("durationInDays").item(0).getTextContent());
 
-                String depositType = eElement.getElementsByTagName("depositType").item(0).getTextContent();
-                String customerNumber = eElement.getElementsByTagName("customerNumber").item(0).getTextContent();
-                String depositBalance = eElement.getElementsByTagName("depositBalance").item(0).getTextContent();
-                String durationInDays = eElement.getElementsByTagName("durationInDays").item(0).getTextContent();
-                Deposit obj = (Deposit) Class.forName(depositType).newInstance();
-                obj.customerNumber = Integer.parseInt(customerNumber);
-                obj.depositeBalance = Integer.parseInt(depositBalance);
-                obj.durationInDays = Integer.parseInt(durationInDays);
+                //Fill the Objects
+                Deposit deposit = new Deposit();
+                deposit.setCustomerNumber(Integer.parseInt(customerNumber));
 
-
-
-/*
-                if (depositType.equals("ShortTerm")) {
-                    Class<?> SampleOfShortTerm = null;
-                    try {
-                        SampleOfShortTerm = Class.forName(depositType);
-                    } catch (Exception e) {
-                        e.printStackTrace(
-                        );
+                try {
+                    deposit.setDepositBalance(new BigDecimal(depositBalance));
+                    int resultOfComparing = deposit.getDepositBalance().compareTo(BigDecimal.ZERO);
+                    if (resultOfComparing <= 0) {
+                        throw new NegativeBalanceException();
                     }
-                    //Create instance of ShortTerm class
-                    ShortTerm shortTerm_object;
-                    try {
-                        shortTerm_object = (ShortTerm) SampleOfShortTerm.newInstance();
-                        shortTerm_object.interestRate = 10;
-                        String customerNumber = eElement.getElementsByTagName("customerNumber").item(0).getTextContent();
-                        String depositBalance = eElement.getElementsByTagName("depositBalance").item(0).getTextContent();
-                        String durationInDays = eElement.getElementsByTagName("durationInDays").item(0).getTextContent();
-                        shortTerm_object.customerNumber = Integer.parseInt(customerNumber);
-                        shortTerm_object.depositeBalance = Integer.parseInt(depositBalance);
-                        shortTerm_object.durationInDays = Integer.parseInt(durationInDays);
-                        System.out.println(shortTerm_object.customerNumber + "  " + shortTerm_object.PayedInterest());
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
+                    deposit.setDurationInDays(Integer.parseInt(durationInDays));
+                    if (deposit.getDurationInDays() <= 0) {
+                        throw new IllegalArgumentException();
                     }
+                    deposit.setDepositType((DepositType) Class.forName(depositType).newInstance());
+                    listForSavingDeposits.add(deposit);
+                } catch (ClassNotFoundException e) {
+                    System.out.println("Invalid Deposit Type");
+                } catch (NegativeBalanceException e) {
+                    System.out.println("Invalid Deposit Balance");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Invalid DurationInDays");
                 }
-
-                if (depositType.equals("Qarz")) {
-                    Class<?> SampleOfQarz = null;
-                    try {
-                        SampleOfQarz = Class.forName("Qarz");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    //Create instance Of Qarz
-                    Qarz qarz_object = null;
-                    try {
-                        qarz_object = (Qarz) SampleOfQarz.newInstance();
-                        qarz_object.interestRate = 0;
-                        qarz_object.customerNumber = Integer.parseInt(eElement.getElementsByTagName("customerName").item(0).getTextContent());
-                        qarz_object.depositeBalance = Integer.parseInt(eElement.getElementsByTagName("depositBalance").item(0).getTextContent());
-                        qarz_object.durationInDays = Integer.parseInt(eElement.getElementsByTagName("durationInDays").item(0).getTextContent());
-                    } catch (InstantiationException | IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }//End Of Qarz
-
-                if (depositType.equals("LongTerm")) {
-                    Class<?> SampleOfLongTerm = null;
-                    try {
-                        SampleOfLongTerm = Class.forName("LongTerm");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    //Create instance of LongtTerm
-                    LongTerm longTerm_object = null;
-                    try {
-                        longTerm_object = (LongTerm) SampleOfLongTerm.newInstance();
-                        longTerm_object.interestRate = 20;
-                        longTerm_object.customerNumber = Integer.parseInt(eElement.getElementsByTagName("customerName").item(0).getTextContent());
-                        longTerm_object.depositeBalance = Integer.parseInt(eElement.getElementsByTagName("depositBalance").item(0).getTextContent());
-                        longTerm_object.durationInDays = Integer.parseInt(eElement.getElementsByTagName("durationInDays").item(0).getTextContent());
-                    } catch (InstantiationException e) {
-                        e.printStackTrace();
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }
-*/
-
             }
-        }//EndOf for
+        }
+
+        return listForSavingDeposits;
+    }
 
 
-        return doc;
+    //Write objects in output file
+    public void WriteInRandomAccessFile(List<Deposit> inputDepositList) throws IOException {
+        RandomAccessFile output = new RandomAccessFile("output.txt", "rw");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Deposit deposit : inputDepositList) {
+            stringBuilder.append(deposit.getCustomerNumber()).append("#").append(deposit.getPayedInterest().toString()).append("\n");
+        }
+        output.writeBytes(stringBuilder.toString());
     }
 }
